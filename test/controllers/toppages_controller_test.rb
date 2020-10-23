7eee3c525bf8f7aba0769e6829a9e486a39ed1bc
keisuke-sakuma <sakusuke0412@gmail.com>
2020-10-23 20:25:27 +0900

    RSpecの設定

--- test/controllers/toppages_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ToppagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get toppages_home_url
-    assert_response :success
-  end
-
-end
